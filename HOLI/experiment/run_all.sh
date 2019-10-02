817754ee74c8a028f64956c4e47a4dd125d1bac4
Yu-Yang Lin <LaifsV1@gmail.com>
2019-10-02 14:27:45 +0100

    deleted unecessary files

--- HOLI/experiment/run_all.sh
+++
@@ -1,8 +0,0 @@
-for file in programs/safe/equivalence/*.holi; do echo $file; krun --search --pattern "<k> fail </k> <trace> T </trace>" $file; done
-
-for file in programs/safe/*.holi; do echo $file; krun --search --pattern "<k> fail </k> <trace> T </trace>" $file; done
-
-for file in programs/unsafe/equivalence/*.holi; do echo $file; krun --search --pattern "<k> fail </k> <trace> T </trace>" $file; done
-
-for file in programs/unsafe/*.holi; do echo $file; krun --search --pattern "<k> fail </k> <trace> T </trace>" $file; done
-
