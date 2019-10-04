914468bd936d68eefddf3ec266d6d4a7ba097a22
Yu-Yang Lin <LaifsV1@gmail.com>
2019-10-04 14:46:05 +0100

    preprocessed final table

--- HOLi/experiment/files/out/pre1/create_files.sh
+++ HOLi/experiment/files/out/pre1/create_files.sh
@@ -3,7 +3,8 @@
 final=final/final.csv
 rm -f $final
 
-for file in awkward_2 awkward_3 awkward_safe call_no_order call_order cross_reentrancy cross_reentrancy_safe dos_safe double_alloc_1 double_alloc_2 double_alloc_simple double_free double_free_simple file_lock file_lock_param file_write flat_combiner imj_inp11 lock_counter_safe mochi optimistic_safe read reentrancy_2 reentrancy reentrancy_param reentrancy_safe remote_sync_1 remote_sync_2 reservation reverse_counter_safe
+#for i in ??????*; do echo "${i%??????}"; done | sort -u | xargs
+for file in awkward_2 awkward_2-mo awkward_3 awkward_3-mo awkward_safe awkward_safe-mo call_no_order call_no_order-mo call_order call_order-mo call_order_param call_order_param-mo cross_reentrancy cross_reentrancy-mo cross_reentrancy_safe cross_reentrancy_safe-mo dos_safe dos_safe-mo double_alloc_1 double_alloc_1-mo double_alloc_2 double_alloc_2-mo double_alloc_simple double_alloc_simple-mo double_free double_free-mo double_free_simple double_free_simple-mo file_lock file_lock-mo file_lock_param file_lock_param-mo file_write file_write-mo flat_combiner flat_combiner-mo imj_inp11 imj_inp11-mo lock_counter_safe lock_counter_safe-mo optimistic_safe optimistic_safe-mo read read-mo reentrancy reentrancy_2 reentrancy_2-mo reentrancy-mo reentrancy_param reentrancy_param-mo reentrancy_safe reentrancy_safe-mo remote_sync_1 remote_sync_1-mo remote_sync_2 remote_sync_2-mo reservation reservation-mo reverse_counter_safe reverse_counter_safe-mo small_various_1 small_various_2 small_various_3 small_various_4 various_mains various_mains_no_equiv various_safe various_safe_no_awk various_unsafe various_unsafe_no_equiv
 do
   printf $file , >> $final
   for i in 2 3 4 5
